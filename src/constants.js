d1b6f59555c8031c32c74d72c5450bfbad9a57eb
Simon Huang <me@simonh.io>
2022-07-24 22:31:08 -0700

    Removed excess files, added favicon

--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const DISTANCE = 1000;
--- src/style/colors.js
+++
@@ -1,5 +0,0 @@
-export const primary = '#38C4E6';
-export const secondary = '#E66143';
-export const secondary2 = '#994734';
-export const tertiary = '#E6B02C';
-export const tertiary2 = '#997315';
